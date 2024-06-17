414e431c1e030e085b1da90902507d70af5bf3bf
JuMoth <tkdlqjqkd@gmail.com>
2024-06-17 16:45:15 +0900

    Feat: Animation Event

--- anievent.js
+++ anievent.js
@@ -1,15 +1,31 @@
 (function() {
     const ballElem = document.querySelector('.ball');
 
-    window.addEventListener('click', (e) => {
-        ballElem.style.transform = 'translate(' + (e.clientX-15) + 'px, ' + (e.clientY-15) + 'px)';
-    })
+    // window.addEventListener('click', (e) => {
+    //     ballElem.style.transform = 'translate(' + (e.clientX-15) + 'px, ' + (e.clientY-15) + 'px)';
+    // })
 
-    ballElem.addEventListener('transitionend', (e) => {
-        ballElem.classList.add('end');
-        console.log(e.elapsedTime);
-        console.log(e.propertyName);
-    });
+    // ballElem.addEventListener('transitionend', (e) => {
+    //     ballElem.classList.add('end');
+    //     console.log(e.elapsedTime);
+    //     console.log(e.propertyName);
+    // });
     // transitionend
     // transitionstart
+
+    ballElem.addEventListener('click', () => {
+        ballElem.style.animation = 'ball-ani 1s 3 forwards';
+    });
+
+    ballElem.addEventListener('animationend', () => {
+        ballElem.classList.add('end');
+    });
+
+    ballElem.addEventListener('animationiteration', () => {
+        console.log('반복!');
+    })
+
+    // animationstart
+    // animationend
+    // animationiteration
 })();
